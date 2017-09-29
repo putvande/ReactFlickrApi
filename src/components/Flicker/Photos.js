c77036c91698712316c04089421b72afa8450d0b
WEdwards <WEdwards@sk.ibm.com>
2017-09-29 16:46:19 +0200

    a change in props

--- src/components/Flicker/Photos.js
+++ src/components/Flicker/Photos.js
@@ -6,20 +6,32 @@ class Photos extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            isBeingRender: true,
+        };
         this.handleLayoutComplete = this.handleLayoutComplete.bind(this);
     }
 
+    componentWillReceiveProps(nextProps) {
+        this.setState({ isBeingRender: true})
+    }
     // Masonry fires this function after the layout is performed
     handleLayoutComplete() {
         // initially hidden prevent flash of content before masonry has fixed it &hide loading spinner
-        document.querySelector('.photoContainer').style.visibility='visible';
-        document.querySelector('.fa-circle-o-notch').style.display='none';
+        // document.querySelector('.photoContainer').style.visibility='visible';
+        // document.querySelector('.fa-circle-o-notch').style.display='none';
+        if(this.state.isBeingRender) {
+            this.setState({ isBeingRender: false });
+            document.querySelector('.photoContainer').style.visibility='visible';
+            document.querySelector('.fa-circle-o-notch').style.display='none';
+        }
     }
 
     render() {
 		// results will either be the first time loaded results or the search query, otherwise empty
-		const results = this.props.searchResults || this.props.items || [];
-        return <Masonry onLayoutComplete={ this.handleLayoutComplete } className="masonryLayout" ref={function(c) {this.masonry = this.masonry || c.masonry;}.bind(this)}>
+        const results = this.props.searchResults || this.props.items || [];
+        const { isBeingRender = false } = this.state;
+        return <Masonry onLayoutComplete={ this.handleLayoutComplete } className={'masonryLayout' + (isBeingRender ? 'hide' : '')} ref={function(c) {this.masonry = this.masonry || c.masonry;}.bind(this)}>
                     {results.map((t, i) =>
                         <Photo 
                             key={i}
